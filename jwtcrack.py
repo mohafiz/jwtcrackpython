5c33903996699796a6b1eb9a48508972e5fee72f
Mohamed Hafiz <36772177+mohafiz@users.noreply.github.com>
2018-10-26 09:24:28 +0200

    Add files via upload

--- jwtcrack.py
+++ jwtcrack.py
@@ -5,14 +5,14 @@ from termcolor import colored
 if len(sys.argv) != 3:
       print 'Usage: python {} {} {}'.format(sys.argv[0], 'encoded_jwt', 'dictionary_list')
       sys.exit(1)
-else:
-      encoded = sys.argv[1]
 
-      with open('rockyou.txt', 'r') as dic:
-         for secret in dic:
-            try:
-               jwt.decode(encoded, secret, algorithms=['HS256'])
-               print colered('secret is ' + secret, 'green')
-	       sys.exit(0)
-            except:
-               print colored('Error in decoding JWT !', 'red')
+encoded = sys.argv[1]
+
+with open('rockyou.txt', 'r') as dic:
+   for secret in dic:
+      try:
+         jwt.decode(encoded, secret, algorithms=['HS256'])
+         print colered('secret is ' + secret, 'green')
+	 sys.exit(0)
+      except:
+         print colored('Error in decoding JWT !', 'red')
